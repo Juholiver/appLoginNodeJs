874f152d7417839fff455e422cc698e764bf36e9
Juholiver <juholiver3@gmail.com>
2026-02-05 15:01:39 -0300

    adcionando a conexao com banco atlas

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const connectDB = async () => {
+  
+  try{
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB');
+  }
+  catch(error)
+  {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+connectDB();
 
-app.use(cors());
+
 
 app.get('/', (req, res) => {
   
